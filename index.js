39b3b462badc921256a53f94b943a8507641c6b9
Al Zimmerman <auraelius@gmail.com>
2020-06-09 14:27:10 -0700

    initial functionality

---
+++ index.js
@@ -0,0 +1,68 @@
+/* eslint-disable no-console */
+'use strict';
+
+// put your own value below!
+const apiKey = 'laq6kPBucnVSuO3cAxupE9Ncnta6X80Dxzd1VybG'; 
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $('#results-list').empty();// if there are previous results, remove them
+  // iterate through the items array
+  for (let i = 0; i < responseJson.data.length; i++){
+    // for each object in the items 
+    //array, add a list item to the results 
+    //list with the  title, description,
+    //and first image
+    $('#results-list').append(
+      `<li><h3>${responseJson.data[i].fullName}</h3>
+      <p>${responseJson.data[i].description}</p>
+      <img src='${responseJson.data[i].images[0].url}'>
+      </li>`
+    )};
+  //display the results section  
+  $('#results').removeClass('hidden');
+};
+
+function getParks(query, maxResults=10) {
+  const params = {
+    api_key: apiKey,
+    q: query,
+    limit: maxResults,
+    type: 'video'
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
+    getParks(searchTerm, maxResults);
+  });
+}
+
+$(watchForm);
